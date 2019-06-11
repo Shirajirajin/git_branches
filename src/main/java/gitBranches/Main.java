409f5717cf464e48819554a9fa65fdc690e5c0b4
Juliusz Waluszewski <shirajira@gmail.com>
2019-06-11 18:15:43 +0200

    dodano plik

--- src/main/java/gitBranches/Main.java
+++ src/main/java/gitBranches/Main.java
@@ -1,13 +1,21 @@
 package gitBranches;
 
+import java.io.*;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 
+        try {
+            PrintWriter printWriter = new PrintWriter(new FileWriter("data.txt", true));
 
+            printWriter.println("Hello world!");
 
+            printWriter.close();
+        } catch (IOException e) {
+            e.printStackTrace();
 
+        }
     }
-
-
 }
