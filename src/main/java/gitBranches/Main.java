45c60b33160dc19a11ff137d3d8959797e618efa
Juliusz Waluszewski <shirajira@gmail.com>
2019-06-11 18:36:43 +0200

    dodano liczenie linni

--- src/main/java/gitBranches/Main.java
+++ src/main/java/gitBranches/Main.java
@@ -1,13 +1,42 @@
 package gitBranches;
 
+import java.io.*;
+
 public class Main {
 
     public static void main(String[] args) {
 
+        try {
+            PrintWriter printWriter = new PrintWriter(new FileWriter("data.txt", true));
+            printWriter.println("Hello world!");
+
+            printWriter.close();
+        } catch (IOException e) {
+            e.printStackTrace();
 
 
+        }
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader("data.txt"));
+            String linia;
+            int licznik =0;
+
+            while ((linia = reader.readLine()) != null) {
+//                String[] slowa = linia.split("%==%");
+//                System.out.println(slowa[1]);
+                licznik++;
+            }
+
+            System.out.println("liczba linni " + licznik );
+            reader.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+
+
+
+
+        }
 
     }
-
-
 }
